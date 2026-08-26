37c7266ae1899163b94f5e920f4ab6f1a0484a4b
sakura <3329518468@qq.com>
2026-08-26 10:51:56 +0800

    Delete src/main directory

--- src/main/java/com/itcast/taliswebmanagement/service/EmpLogService.java
+++
@@ -1,8 +0,0 @@
-package com.itcast.taliswebmanagement.service;
-
-import com.itcast.taliswebmanagement.pojo.EmpLog;
-
-public interface EmpLogService {
-    //记录新增员工日志
-    public void insertLog(EmpLog empLog);
-}
--- src/main/java/com/itcast/taliswebmanagement/service/OperateLogService.java
+++
@@ -1,7 +0,0 @@
-package com.itcast.taliswebmanagement.service;
-
-import com.itcast.taliswebmanagement.pojo.PageResult;
-
-public interface OperateLogService {
-    PageResult page(Integer page, Integer pageSize);
-}
